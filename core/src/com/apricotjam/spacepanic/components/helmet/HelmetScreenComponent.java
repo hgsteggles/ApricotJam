bce1dc85e667d87f3dc864e19c4f230bb7568a26
Ridoncules <hgsteggles@gmail.com>
2016-01-10 14:29:32 +0000

    Improved LED message interface.

--- core/src/com/apricotjam/spacepanic/components/helmet/HelmetScreenComponent.java
+++ core/src/com/apricotjam/spacepanic/components/helmet/HelmetScreenComponent.java
@@ -1,16 +1,26 @@
 package com.apricotjam.spacepanic.components.helmet;
 
 import com.apricotjam.spacepanic.gameelements.Resource;
+import com.apricotjam.spacepanic.systems.helmet.HelmetSystem;
+import com.apricotjam.spacepanic.systems.helmet.HelmetWorld;
 import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.utils.ObjectMap;
+import com.badlogic.gdx.utils.Queue;
 
 public class HelmetScreenComponent implements Component {
 	public ObjectMap<Resource, Integer> resourceCount = new ObjectMap<Resource, Integer>();
+	public ObjectMap<Resource, Integer> maxCount = new ObjectMap<Resource, Integer>();
+	public Queue<HelmetSystem.LED_Message> messages = new Queue<HelmetSystem.LED_Message>();
 	
 	public HelmetScreenComponent() {
-		resourceCount.put(Resource.OXYGEN, 25);
-		resourceCount.put(Resource.OIL, 20);
-		resourceCount.put(Resource.RESOURCE2, 15);
-		resourceCount.put(Resource.RESOURCE3, 10);
+		maxCount.put(Resource.OXYGEN, 4*HelmetWorld.FLUID_SEG_RSRC_COUNT);
+		maxCount.put(Resource.OIL, 3*HelmetWorld.FLUID_SEG_RSRC_COUNT);
+		maxCount.put(Resource.RESOURCE2, 2*HelmetWorld.FLUID_SEG_RSRC_COUNT);
+		maxCount.put(Resource.RESOURCE3, 1*HelmetWorld.FLUID_SEG_RSRC_COUNT);
+		
+		resourceCount.put(Resource.OXYGEN, 4*HelmetWorld.FLUID_SEG_RSRC_COUNT);
+		resourceCount.put(Resource.OIL, 3*HelmetWorld.FLUID_SEG_RSRC_COUNT);
+		resourceCount.put(Resource.RESOURCE2, 2*HelmetWorld.FLUID_SEG_RSRC_COUNT);
+		resourceCount.put(Resource.RESOURCE3, 1*HelmetWorld.FLUID_SEG_RSRC_COUNT);
 	}
 }
