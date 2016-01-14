d2127116d3f75dc91149119e1de989c1d31ffbe8
Ridoncules <hgsteggles@gmail.com>
2016-01-14 14:00:01 +0000

    Game over screen starts spinning the star background.

--- core/src/com/apricotjam/spacepanic/screen/GameOverScreen.java
+++ core/src/com/apricotjam/spacepanic/screen/GameOverScreen.java
@@ -1,11 +1,16 @@
 package com.apricotjam.spacepanic.screen;
 
 import com.apricotjam.spacepanic.SpacePanic;
+import com.apricotjam.spacepanic.art.MiscArt;
 import com.apricotjam.spacepanic.components.BitmapFontComponent;
 import com.apricotjam.spacepanic.components.ComponentMappers;
+import com.apricotjam.spacepanic.components.MovementComponent;
+import com.apricotjam.spacepanic.components.ScrollComponent;
+import com.apricotjam.spacepanic.components.TextureComponent;
 import com.apricotjam.spacepanic.components.TransformComponent;
 import com.apricotjam.spacepanic.components.TweenComponent;
 import com.apricotjam.spacepanic.components.TweenSpec;
+import com.apricotjam.spacepanic.components.TweenSpec.Cycle;
 import com.apricotjam.spacepanic.interfaces.TweenInterface;
 import com.apricotjam.spacepanic.systems.ClickSystem;
 import com.apricotjam.spacepanic.systems.MovementSystem;
@@ -13,6 +18,8 @@ import com.apricotjam.spacepanic.systems.RenderingSystem;
 import com.apricotjam.spacepanic.systems.ScrollSystem;
 import com.apricotjam.spacepanic.systems.TweenSystem;
 import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Interpolation;
 
 public class GameOverScreen extends BasicScreen {
@@ -26,6 +33,7 @@ public class GameOverScreen extends BasicScreen {
 		add(new MovementSystem());
 		add(new ScrollSystem());
 		
+		addMovementScroll(backgroundEntity);
 		add(backgroundEntity);
 		add(createGameOver());
 	}
@@ -63,6 +71,33 @@ public class GameOverScreen extends BasicScreen {
 		entity.add(tweenComponent);
 		
 		return entity;
+	}
+	
+	private void addMovementScroll(Entity entity) {
+		MovementComponent movementComp = new MovementComponent();
+		movementComp.rotationalVelocity = 5.0f;
+		entity.add(movementComp);
+		
+		ScrollComponent scrollComp = new ScrollComponent();
+		scrollComp.speed.x = 0.5f;
+		entity.add(scrollComp);
+		
+		TweenComponent tweenComp = new TweenComponent();
+		TweenSpec tweenSpec = new TweenSpec();
+		tweenSpec.start = 0f;
+		tweenSpec.end = 1f;
+		tweenSpec.period = 2f;
+		tweenSpec.cycle = Cycle.ONCE;
+		tweenSpec.interp = Interpolation.linear;
+		tweenSpec.tweenInterface = new TweenInterface() {
+			@Override
+			public void applyTween(Entity e, float a) {
+				ComponentMappers.movment.get(e).rotationalVelocity = 5.0f*a;
+				ComponentMappers.scroll.get(e).speed.x = 0.5f*a;
+			}
+		};
+		tweenComp.tweenSpecs.add(tweenSpec);
+		entity.add(tweenComp);
 	}
 
 	@Override
