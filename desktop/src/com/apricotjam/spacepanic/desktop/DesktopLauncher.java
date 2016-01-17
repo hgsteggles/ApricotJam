e2959f05851055f6c6d5f351cf35da97cdd3de48
drumber-1 <drumber01@gmail.com>
2016-01-17 20:20:33 +0000

    Reverted background to non-test version

--- desktop/src/com/apricotjam/spacepanic/desktop/DesktopLauncher.java
+++ desktop/src/com/apricotjam/spacepanic/desktop/DesktopLauncher.java
@@ -3,7 +3,6 @@ package com.apricotjam.spacepanic.desktop;
 import com.apricotjam.spacepanic.SpacePanic;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 
 public class DesktopLauncher {
 	public static void main(String[] arg) {
