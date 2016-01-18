48e425b9bada9b273038c739a38c67d6bbf8330c
drumber-1 <drumber01@gmail.com>
2016-01-18 19:19:19 +0000

    Hide navigation bar and status bar

--- android/src/com/apricotjam/spacepanic/AndroidLauncher.java
+++ android/src/com/apricotjam/spacepanic/AndroidLauncher.java
@@ -14,6 +14,7 @@ public class AndroidLauncher extends AndroidApplication {
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		config.useAccelerometer = false;
 		config.useCompass = false;
+		config.useImmersiveMode = true;
 		initialize(new SpacePanic(), config);
 	}
 }
